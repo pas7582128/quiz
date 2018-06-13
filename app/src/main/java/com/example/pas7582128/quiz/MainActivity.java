b3771e9db48e9e9a9236cd4086833ac26ab53fb2
Akash <pas7582128@gmail.com>
2018-06-13 11:51:56 +0530

    c3

--- app/src/main/java/com/example/pas7582128/quiz/MainActivity.java
+++ app/src/main/java/com/example/pas7582128/quiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pas7582128.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private static final String KEY_INDEX = "index";
     private Question[] mQuestionBank=new Question[]{
             new Question(R.string.question_australia,
                     true),
@@ -28,19 +30,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        }
         mTrueButton=findViewById(R.id.true_button);
         mFalseButton=findViewById(R.id.false_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
-            
+            checkAnswer(true);
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
         mQuestionTextView=(TextView) findViewById(R.id.question_textview);
@@ -61,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
         });
         upDateQuestion();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("AA","onSaveInstanceState");
+        outState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
     private void upDateQuestion(){
         int question=mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
